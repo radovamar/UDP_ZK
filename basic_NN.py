10d91b2ba31a0a2cbdb2cee01764d130b590ff04
Martina Řádová <radovamar@natur.cuni.cz>
2022-02-09 21:56:11 +0100

    Dve zakladni NN - dataset MNIST - zakladni cislice a mnist_784, pred zaverecnou kontrolou, nutne komentare a dokumentace

--- basic_NN.py
+++ basic_NN.py
@@ -1,40 +1,44 @@
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.exceptions import ConvergenceWarning
+import warnings
+from sklearn.metrics import  confusion_matrix as c_m
 
-
-X, y = load_digits(return_X_y = True) # tridy??
-
+X, y = load_digits(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2)
-mlp = MLPClassifier(max_iter = 1000, hidden_layer_sizes = (100, 80), alpha = 0.001, solver = "adam", random_state = 2)
+
+iterace = int(input("Zadej celé číslo větší než 200:"))
+solv = str(input("Vyber zkratku: adam, sgd, lbfgs" ))
+
+mlp = MLPClassifier(random_state=2, max_iter = iterace, solver = solv) #, hidden_layer_sizes = (vrstvy, uzly), alpha = 0.001, solver = solv, random_state = 2)
 mlp.fit(X_train, y_train)
+# this example won't converge because of resource usage constraints on
+# our Continuous Integration infrastructure, so we catch the warning and
+# ignore it here
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
+    mlp.fit(X_train, y_train)
+
+
+
+print('0-9 digits recognition score (MNIST):',mlp.score(X_test, y_test))
 
 y_pred = mlp.predict(X_test)
 incorrect = X_test[y_pred != y_test]
 incorrect_true = y_test[y_pred != y_test]
 incorrect_pred = y_pred[y_pred != y_test]
 
-plt.matshow(X[0].reshape(8, 8), cmap = plt.cm.gray)
-plt.xticks(()) # remove x tick marks
-plt.yticks(()) # remove y tick marks
-plt.show()
+cm = c_m(y_test, y_pred)
+actual = '  actual  '
+predic = '            p r e d i c t e d '
+labels = '      0  1  2  3  4  5  6  7  8  9'
 
-j = 0
-plt.matshow(incorrect[j].reshape(8, 8), cmap = plt.cm.gray)
-plt.xticks(())
-plt.yticks(())
-plt.show()
-print("true value:", incorrect_true[j])
-print("predicted value:", incorrect_pred[j])
+print()
+print(' '*11, 'confusion matrix')
+print()
+print(predic)
+print(labels)
 
-x = X_test[5]
-
-print(X.shape, y.shape)
-print(X[0].reshape(8, 8))
-
-print(mlp.predict([x]))
-print(f"I predict that the number is... {mlp.predict([x])[0]}.")
-print("accuracy:", mlp.score(X_test, y_test))
-
+for row in range(10):
+    print(actual[row], row, cm[row])
